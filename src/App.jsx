363cd8a4974d8283a3937f2c1788ff3a983759f6
barryspacezero <barryspace01@gmail.com>
2026-04-21 00:49:08 +0530

    updated projects with a new one :)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import Nav from "./sections/Nav"
 import GameInfoBot from "./assets/GameInfoBot.png"
 import FlipLearn from "./assets/FlipLearn.png"
+import startpage from "./assets/start-page.png"
 import Exp from "./sections/Exp"
 import NFP from "./assets/NFP.png"
 import taskmanager from "./assets/taskmanager.png"
@@ -58,10 +59,12 @@ function App() {
       <h1 id="projects" className="text-5xl font-bold text-white justify-center flex border border-theme-border mx-4 p-4 md:mx-8">Projects</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 mx-4 md:mx-8">
 
+        <ProjectCard src={startpage} title="RadioGuessr" description="I built a webgame in which you guess the location of a live radio stream on a 3D globe." projectLink="https://radioguessr.space/" githubLink={"https://github.com/barryspacezero/RadioGuessr"} />
         <ProjectCard src={NFP} title="Food Inspector" description="A simple nutrition tracking app built with ReactJS and Tailwind CSS." projectLink="https://nutrition-fact-panels.vercel.app/" githubLink={"https://github.com/barryspacezero/Nutrition_Fact_Panels"} />
         <ProjectCard src={taskmanager} title="Task-Tracker" description="A Task Tracker app built with NextJS, TypeScript and Tailwind CSS with backend written in Express, NodeJS and MongoDB." projectLink="https://penguin-task-tracker.vercel.app/" githubLink={"https://github.com/barryspacezero/Task-Tracker"} />
         <ProjectCard src={GameInfoBot} title="Telegram-GameInfoBot" description="A Telegram bot built with Python using the Pyrogram library and Twitch's API." githubLink="https://github.com/barryspacezero/Telegram-GameInfoBot" />
-        <ProjectCard src={FlipLearn} title="Flip-Learn" description="A Flashcards app for Quizzes built with ReactJS, TypeScript and Tailwind CSS." projectLink="https://flip-learn-xw4f.vercel.app/" githubLink={"https://github.com/barryspacezero/Fliplearn"} />
+
+
       </div>
       <div className="mb-10">
         <About />
